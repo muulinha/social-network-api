d2a49d98c9208dc5d1414cf9493cefd6eeaafd18
muulinha <lucasfl.araujo@hotmail.com>
2023-04-01 08:14:20 +0800

    Initial Commit with some work prior creating repo

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  username : {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: '/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/'
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `commentCount` that gets the amount of comments per user
+userSchema
+  .virtual('fullName')
+  // Getter
+  .get(function () {
+    return `${this.first} ${this.last}`;
+  })
+  // Setter to set the first and last name
+  .set(function (v) {
+    const first = v.split(' ')[0];
+    const last = v.split(' ')[1];
+    this.set({ first, last });
+  });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
